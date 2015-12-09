a661ddf6bf088ddeedb1990b3e3e10a0ac860529
Menna CC <mkudo94@gmail.com>
2015-12-09 21:36:06 +0200

    UI custom control

---
+++ WindowsFormsControlLibrary1/WindowsFormsControlLibrary1/UserControl1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsControlLibrary1
+{
+    partial class TestingUserControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.ovalShape2 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.RosyBrown;
+            this.label1.Location = new System.Drawing.Point(60, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gate";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape2,
+            this.ovalShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(150, 150);
+            this.shapeContainer1.TabIndex = 1;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.ovalShape1.Location = new System.Drawing.Point(27, 33);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(11, 12);
+            this.ovalShape1.Click += new System.EventHandler(this.ovalShape1_Click);
+            // 
+            // ovalShape2
+            // 
+            this.ovalShape2.Location = new System.Drawing.Point(29, 104);
+            this.ovalShape2.Name = "ovalShape2";
+            this.ovalShape2.Size = new System.Drawing.Size(11, 12);
+            // 
+            // TestingUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "TestingUserControl";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TestingUserControl_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TestingUserControl_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape2;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+    }
+}
---
+++ WindowsFormsControlLibrary1/WindowsFormsControlLibrary1/UserControl1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class TestingUserControl: UserControl
+    {
+        public TestingUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private Point MouseDownLocation;
+
+        private void TestingUserControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                MouseDownLocation = e.Location;
+            }
+        }
+
+        private void TestingUserControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                this.Left = e.X + this.Left - MouseDownLocation.X;
+                this.Top = e.Y + this.Top - MouseDownLocation.Y;
+            }
+            //Graphics g = this.CreateGraphics();
+            //g.DrawLine(Pens.Black, MouseDownLocation, e.Location);
+        }
+
+        private void ovalShape1_Click(object sender, EventArgs e)
+        {
+
+            if(ovalShape1.BackColor == Color.Green)
+            {
+                ovalShape1.BackColor = Color.Red;
+
+            }
+            else
+            {
+                ovalShape1.BackColor = Color.Green;
+
+            }
+        }
+    }
+}
