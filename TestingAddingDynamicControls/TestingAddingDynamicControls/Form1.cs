9184ea540699a8d0e7aa1dd3fedba3093238f179
Menna CC <mkudo94@gmail.com>
2015-12-10 14:33:08 +0200

    ading the Main UI Form project

---
+++ TestingAddingDynamicControls/TestingAddingDynamicControls/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsControlLibrary1;
+
+namespace TestingAddingDynamicControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<TestingUserControl> buttons = new List<TestingUserControl>();
+            
+                TestingUserControl newButton = new TestingUserControl();
+                newButton.BackColor = Color.Pink;
+                buttons.Add(newButton);
+                this.Controls.Add(newButton);
+        }
+    }
+}
