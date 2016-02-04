17045f056370492ae46bc8a639f42fea3e7e4d71
ShimaaLotfy <meshmesha559@gmail.com>
2016-02-04 04:48:58 -0800

    decent logic simulator

---
+++ Decent Logic Simulator/Decent Logic Simulator/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Decent_Logic_Simulator.FrontEnd;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decent_Logic_Simulator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Point startPoint, endPoint;
+
+        public void DrawOnForm(Point start,DecentUserControl SenderCntrl)
+        {
+            this.startPoint.X = start.X + SenderCntrl.Location.X;
+            this.startPoint.Y = start.Y + SenderCntrl.Location.Y;
+            String txt = startPoint.X.ToString() + " " + startPoint.Y.ToString();
+            SingletonForm.getFormInstance().textBoxLocation.Text = txt;
+
+        }
+        private void buttonGate_Click(object sender, EventArgs e)
+        {
+            //<summary>
+            //// creates Gate_front object that creates a Gate object of type suitable to the name of the button
+            //// for example a button Named "OR" will create an OR gate and call its draw function
+            //// also that class is responsible for handling interactions with the gate object from the front end.
+            //</summary>
+            Gate_Front newGate = new Gate_Front(ActiveControl.Name.ToString());
+            this.Controls.Add(newGate);
+
+        }
+        private void Switch_Click(object sender, EventArgs e)
+        {
+            Input_Front s = new Input_Front();
+            this.Controls.Add(s);
+        }
+        private void LED_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawLine(SystemPens.ControlText, startPoint, endPoint);
+        }
+        public void DrawWire(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && startPoint != null)
+                endPoint = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string txt = e.X.ToString() + " " + e.Y.ToString();
+            textBoxLocationEnd.Text = txt;
+            DrawWire(e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+    }
+}
---
+++ Decent Logic Simulator/Decent Logic Simulator/FrontEnd/Gate_Front.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OUR_LogicSimulator;
+
+namespace Decent_Logic_Simulator.FrontEnd
+{
+    public partial class Gate_Front : DecentUserControl
+    {
+        public Gate_Front(string gateType)
+        {
+            InitializeComponent();
+            this.gateType = gateType;
+        }
+
+        #region Private Attributes
+        private string gateType = null;
+        #endregion
+
+        private void createGateDueToType(object sender, PaintEventArgs e)
+        {
+
+            //switch can be replaced by a one line of code using runtime Creation ((gotta GOOGLE it))
+            Gate g;
+            switch (gateType)
+            {
+                case "OR":
+                    g = new OR();
+                    break;
+                case "NOT":
+                    g = new NOT();
+                    break;
+                case "AND":
+                    g = new AND();
+                    break;
+                case "NAND":
+                    g = new NAND();
+                    break;
+
+                default:
+                    g = null;
+                    break;
+            }
+            g.Draw(sender, e);
+
+        }
+
+    }
+}
